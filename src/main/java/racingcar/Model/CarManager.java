a8535089512c2464f606ba7838847ef4efe28e4b
Jimin <gift040404@naver.com>
2025-10-24 20:27:31 +0900

    feat(CarManager): Manage car creation and validation
    - create Car instances
    - validate name length
    - check for duplicate car names
    - track total number of cars

---
+++ src/main/java/racingcar/Model/CarManager.java
@@ -0,0 +1,71 @@
+package racingcar.Model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class CarManager {
+    private final List<Car> cars;
+    private int totalCar;
+
+    public CarManager() {
+        this.cars = new ArrayList<>();
+    }
+
+    public void manageCars(String carName) {
+        createEachCarInstance(carName);
+        checkDuplicatedName();
+        findTotalCar();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public int getTotalCarNum() {
+        return totalCar;
+    }
+
+    private void createEachCarInstance(String carName){
+        if (isNameEmpty(carName)){
+            throw new IllegalArgumentException();
+        }
+        parseCarNameByComma(carName);
+    }
+
+    private boolean isNameEmpty(String carName){
+        return carName == null;
+    }
+
+    private void parseCarNameByComma(String carName) {
+        StringTokenizer tokenizer = new StringTokenizer(carName, ",");
+        while (tokenizer.hasMoreTokens()) {
+            String tmpName = tokenizer.nextToken();
+
+            if (tmpName.length() > 5) {
+                throw new IllegalArgumentException();
+            }
+
+            Car car = new Car(tmpName);
+            this.cars.add(car);
+        }
+    }
+
+    private void checkDuplicatedName(){
+        Iterator<Car> iterator = cars.iterator();
+        Set<String> duplicatedNames = new HashSet<>();
+        while (iterator.hasNext()) {
+            duplicatedNames.add(iterator.next().getName());
+        }
+        if (duplicatedNames.size() != cars.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void findTotalCar(){
+        this.totalCar = this.cars.size();
+    }
+}
