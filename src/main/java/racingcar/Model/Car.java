fa402e4bc9c48a9f7f37176f1cee0873276db054
Jimin <gift040404@naver.com>
2025-10-24 20:30:24 +0900

    feat(Car): Add field and implement compareTo
    - add randomNumber field
    - implements Comparable and override compareTo

--- src/main/java/racingcar/Model/Car.java
+++ src/main/java/racingcar/Model/Car.java
@@ -1,12 +1,14 @@
 package racingcar.Model;
 
-public class Car {
+public class Car implements Comparable<Car> {
     private final String name;
     private int totalMovingDistance;
+    private int randomNumber;
 
     protected Car (String name) {
         this.name = name;
         this.totalMovingDistance=0;
+        this.randomNumber=0;
     }
 
     public String getName(){
@@ -19,5 +21,18 @@ public class Car {
 
     public int getTotalMovingDistance(){
         return totalMovingDistance;
+    }
+
+    public void setRandomNumber(int number){
+        this.randomNumber=number;
+    }
+
+    public int getRandomNumber(){
+        return randomNumber;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return Integer.compare(car.getTotalMovingDistance(), this.totalMovingDistance);
     }
 }
