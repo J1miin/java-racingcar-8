19241eee58b887d8081688353fa50e481fdd733e
Jimin <gift040404@naver.com>
2025-10-24 20:35:37 +0900

    feat(RacingController): Implement race game flow
    - add CarManager and Race models, and OutputView class
    - implement createCars, startRace, and raceResult methods

--- src/main/java/racingcar/Controller/RacingController.java
+++ src/main/java/racingcar/Controller/RacingController.java
@@ -1,16 +1,25 @@
 package racingcar.Controller;
 
+import racingcar.Model.CarManager;
+import racingcar.Model.Race;
 import racingcar.View.InputView;
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.View.OutputView;
 
 public class RacingController {
     private InputView inputView;
+    private CarManager carManager;
+    private Race race;
+    private OutputView outputView;
 
     private String totalCarNames;
     private int turn;
 
     public RacingController(){
         this.inputView = new InputView();
+        this.carManager = new CarManager();
+        this.race = new Race();
+        this.outputView = new OutputView();
     }
 
     public void run(){
@@ -31,14 +40,21 @@ public class RacingController {
     }
 
     public void createCars(){
-
+        carManager.manageCars(this.totalCarNames);
     }
 
     public void startRace(){
-
+        for (int play = 0 ; play < this.turn ; play++){
+            if (play==0) {
+                outputView.printResultText();
+            }
+            race.play(carManager.getCars(),carManager.getTotalCarNum());
+            outputView.printRacing(carManager.getCars());
+        }
     }
 
     public void raceResult(){
-
+        String winnerName = race.findWinner(carManager.getCars());
+        outputView.printWinner(winnerName);
     }
 }
