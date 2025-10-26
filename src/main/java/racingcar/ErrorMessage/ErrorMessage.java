a0e211537952b6e1648ad3ff2a47de9fd56ffd24
Jimin <gift040404@naver.com>
2025-10-27 01:45:33 +0900

    feat(ErrorMessage): Add ErrorMessage separately

---
+++ src/main/java/racingcar/ErrorMessage/ErrorMessage.java
@@ -0,0 +1,7 @@
+package racingcar.ErrorMessage;
+
+public class ErrorMessage {
+    public static final String EMPTY_INPUT = "입력값이 없습니다.";
+    public static final String EMPTY_CAR_NAME = "차의 이름에 공백이 있을 수 없습니다.";
+    public static final String NAME_LENGTH_OVER = "차의 이름은 5글자를 넘어갈 수 없습니다.";
+}
