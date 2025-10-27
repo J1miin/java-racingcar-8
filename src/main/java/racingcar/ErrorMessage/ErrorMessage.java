1f9c65ba942815c9dcccfd68c1e964111017c4e3
Jimin <gift040404@naver.com>
2025-10-27 15:02:48 +0900

    refactor(ErrorMessage): Add detailed error messages

--- src/main/java/racingcar/ErrorMessage/ErrorMessage.java
+++ src/main/java/racingcar/ErrorMessage/ErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.ErrorMessage;
 
 public class ErrorMessage {
     public static final String EMPTY_INPUT = "입력값이 없습니다.";
-    public static final String EMPTY_CAR_NAME = "차의 이름에 공백이 있을 수 없습니다.";
+    public static final String EMPTY_CAR_NAME = "차의 이름 중에 값이 들어오지 않은 이름이 있습니다.";
     public static final String NAME_LENGTH_OVER = "차의 이름은 5글자를 넘어갈 수 없습니다.";
     public static final String DUPLICATED_CAR_NAME = "자동차의 이름이 중복될 수 없습니다.";
     public static final String TRY_NUMBER_IS_STRING = "게임 시도 횟수로 문자 또는 실수가 입력됐습니다.";
